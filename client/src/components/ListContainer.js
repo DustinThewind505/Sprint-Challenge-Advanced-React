edc1ac0efc39495191deedb535f1499c43298137
unknown <Dustin.Guillen14@yahoo.com>
2020-03-20 19:24:49 -0600

    Answered questions, created class components

---
+++ client/src/components/ListContainer.js
@@ -0,0 +1,36 @@
+import React, { Fragment } from 'react';
+import Card from './Card';
+
+class ListContainer extends React.Component {
+
+    state = {
+        Data: []
+    }
+
+    componentDidMount() {
+        fetch("http://localhost:5000/api/players")
+        .then(response => response.json())
+        .then(response => {
+            this.setState({
+                Data: response
+            })
+        })
+        .catch(err => console.error(err))
+    }
+
+
+    render() {
+        console.log(this.state)
+        return (
+            <Fragment>
+                <h1>Ello Poppet</h1>
+        {this.state.Data.map(element => 
+            element.name
+        )}
+                <Card Data={this.state.Data}/>
+            </Fragment>
+        )
+    }
+}
+
+export default ListContainer;
